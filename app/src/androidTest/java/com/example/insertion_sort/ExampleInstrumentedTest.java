dfcdd18b486be96e7ec38a1725f6d72a181e44d3
ettynan <ettynan@gmail.com>
2024-10-15 12:08:28 -0700

    Innitital commit for new Android project

--- app/src/androidTest/java/com/example/insertion_sort/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/insertion_sort/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.insertionsort;
+package com.example.insertion_sort;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.insertionsort", appContext.getPackageName());
+        assertEquals("com.example.insertion_sort", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/insertionsort/MainActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.insertionsort;
-
-public class MainActivity {
-}
